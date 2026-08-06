cf9b0215e1bf796284dbec6518e426441ce70f31
JavierP <silver9ga@gmail.com>
2026-08-05 23:22:32 -0500

    Started a server with NewServeMux and http.Server

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	//fmt.Println("Hello, World!")
+	mux := http.NewServeMux()
+	httpServer := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	err := httpServer.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
