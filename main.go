732a013e240780508af8cb4b1c83bd81faa09ee2
JavierP <silver9ga@gmail.com>
2026-08-27 22:39:01 -0500

    moved login handler to its own file

--- main.go
+++ main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 	"sync/atomic"
 	"time"
 
-	"github.com/JavierP/Chirpy/internal/auth"
 	"github.com/JavierP/Chirpy/internal/database"
 	"github.com/google/uuid"
 	"github.com/joho/godotenv"
@@ -82,39 +80,4 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-// check for auth
-func (cfg *apiConfig) loginHandler(w http.ResponseWriter, r *http.Request) {
-	type parameters struct {
-		Email    string `json:"email"`
-		Password string `json:"password"`
-	}
-	decoder := json.NewDecoder(r.Body)
-	params := parameters{}
-	err := decoder.Decode(&params)
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Couldn't decode parameters", err)
-		return
-	}
-	user, err := cfg.db.GetUserByEmail(r.Context(), params.Email)
-	if err != nil {
-		respondWithError(w, http.StatusUnauthorized, "Incorrect email or password", err)
-		return
-	}
-	check, err := auth.CheckPasswordHash(params.Password, user.HashedPassword)
-	if err != nil {
-		respondWithError(w, http.StatusUnauthorized, "Incorrect email or password", err)
-		return
-	}
-	if !check {
-		respondWithError(w, http.StatusUnauthorized, "Incorrect email or password", nil)
-		return
-	}
-	respondWithJSON(w, http.StatusOK, User{
-		ID:        user.ID,
-		CreatedAt: user.CreatedAt,
-		UpdatedAt: user.UpdatedAt,
-		Email:     user.Email,
-	})
 }
