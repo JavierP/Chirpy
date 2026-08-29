743e05e70ce3eb90aeca75324b3378bd06c7a490
JavierP <silver9ga@gmail.com>
2026-08-28 23:52:37 -0500

    more jwt

--- internal/auth/jwts.go
+++ internal/auth/jwts.go
@@ -3,8 +3,15 @@ package auth
 import (
 	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
 )
 
 func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"Issuer":    "chirpy-acces",
+		"IssuedAt":  time.Now(),
+		"ExpiresAt": time.Now() + expiresIn,
+		"Subject":   phrases(userID),
+	})
 }
